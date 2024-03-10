815223d7c7b5751d806f9c9e5caa9f04bb59621e
LucSan10 <santiagolucas643@gmail.com>
2024-03-10 20:18:27 -0300

    cria entidade de tarefas
    Co-authored-by: Lucas Bertoldo <BertoLucca@users.noreply.github.com>

---
+++ todo-list/apps/api/src/Models/Task.cs
@@ -0,0 +1,10 @@
+namespace ToDoApp.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
