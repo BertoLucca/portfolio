e41595136be437b6077b0264f0bcabac3191c5a7
LucSan10 <santiagolucas643@gmail.com>
2024-03-10 20:20:47 -0300

    cria contexto de banco
    Co-authored-by: Lucas Bertoldo <BertoLucca@users.noreply.github.com>

---
+++ todo-list/apps/api/src/Models/ToDoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApp.Models;
+
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<ToDoApp.Models.Task> ToDoTasks { get; set; } = null!;
+}
